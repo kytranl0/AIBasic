07d2c13ab982d0e717c5302c1f61599b90eecf89
kytranl0 <kytranl0@yahoo.com>
2022-08-16 19:57:04 -0400

    example added

--- standardScalar/StandardScalar.py
+++ standardScalar/StandardScalar.py
@@ -5,12 +5,16 @@ import numpy as np
 Standard Scalar
 
 (Xi - Xmean) / (std of feature(all elements in array,list))
-TODO: More examples
+
 '''
 
 x = np.array([[-400], [-100], [0], [100], [400]])
-
+x1 = np.array([[1,2,3],
+               [4,5,6],
+               [7,8,9]])
 standardscaler = preprocessing.StandardScaler()
 x_scaler = standardscaler.fit_transform(x)
+x_scaler1 = standardscaler.fit_transform(x1)
 
 print(x_scaler)
+print(x_scaler1)
