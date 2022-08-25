c6af06b6297c5af1bc80620dc2610e759e4267cc
kytranl0 <kytranl0@yahoo.com>
2022-08-25 16:36:31 -0400

    test scenarios

--- standardScalar/trainTestSplit.py
+++ standardScalar/trainTestSplit.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 x = np.arange(1, 25).reshape(12, 2)
-y = np.array([0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0])
+y = np.array([0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0])
 x_train, x_test, y_train, y_test = train_test_split(x, y)
 print("x")
 print(x)
